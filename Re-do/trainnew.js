8806d3d2b2725d4f656564fda51972462fcbb44e
jaustindombrosky <austindombrosky@gmail.com>
2018-03-27 10:21:39 -0600

    new repo

---
+++ Re-do/trainnew.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    var trainData = new Firebase("https://trainproject-38083.firebaseio.com/");
+        $("#addTrain").on("click", function(){
+            var
+        })
