302e4680de07f74d6550e3e6a2fd1bdd794a6d43
jaustindombrosky <austindombrosky@gmail.com>
2018-03-27 16:36:38 -0600

    update

--- Re-do/trainnew.js
+++ Re-do/trainnew.js
@@ -19,5 +19,18 @@ $(document).ready(function(){
             $("#destinationInput").val("")
             $("#tTimeInput").val("")
             $("#frequencyInput").val("")
-
-        })
+            return false;
+    });
+    trainData.on("child_added", function(childSnapshot, prevChildKey){
+        var fbName = childSnapshot.val().name;
+        var fbLine = childSnapshot.val().line;
+        var fbDestination = childSnapshot.val().destination;
+        var fbtTime = childSnapshot.val().tTime;
+        var fbFrequency = childSnapshot.val().frequency;
+        var altTime = moment().diff(moment.unix(fbtTime), "minutes");
+        var tMinus = moment().diff(moment.unix(fbtTime), "minutes") % fbFrequency;
+        var minutes = fbFrequency - timeRemainder;
+        var nextTrain = moment().add(minutes, "m").format("hh:mm A");
+    $("#tTable > tbody").append("<tr><td>" + fbName + "</td><td>" + fbLine + "</td><td>"+ fbDestination + "</td><td>" + fbFrequency + " mins" + "</td><td>" + nextTrain + "</td><td>" + minutes + "</td></tr>");
+    });
+});
